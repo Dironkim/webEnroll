fa4df5758980f811383a797bf41ba198f2371812
aktivgo <AktivGo01062001@gmail.com>
2021-12-10 09:53:18 +0300

    begin enrolle id

--- src/main/java/com/aktivgo/demo/controller/EnrolleeController.java
+++ src/main/java/com/aktivgo/demo/controller/EnrolleeController.java
@@ -1,11 +1,14 @@
 package com.aktivgo.demo.controller;
 
 import com.aktivgo.demo.model.Enrollee;
+import com.aktivgo.demo.model.Exam;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,8 +18,20 @@ public class EnrolleeController {
     public String enrollees(@NotNull Model model){
         model.addAttribute("title","Список абитуриентов");
         List<Enrollee> enrollees = new ArrayList<>();
+        enrollees.add(new Enrollee(1, "Vladislav", LocalDate.parse("2001-06-01")));
+        enrollees.add(new Enrollee(2, "Pavlodar", LocalDate.parse("2000-11-06")));
+        enrollees.add(new Enrollee(3, "Roman", LocalDate.parse("2001-12-30")));
         model.addAttribute("enrollees", enrollees);
         return "enrollees";
+    }
+
+    @GetMapping("/enrollee/{id}")
+    public String enrollee(@PathVariable int id, @NotNull Model model){
+        Enrollee enrollee =
+                model.addAttribute("enrollee", enrollee);
+        ArrayList<Exam> exams = // получение экзаменов, сданных абитуриентом
+                model.addAttribute("exams", exams);
+        return "enrollee";
     }
 
 }
